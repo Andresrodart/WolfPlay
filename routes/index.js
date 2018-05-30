70056f51cddab60dac764bbcef08eb8e87b3bcdc
AndresRodart <arodartel1601@alumno.ipn.mx>
2018-05-29 23:15:47 -0500

    First version

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,18 @@ router.get('/', function(req, res, next) {
     //res.sendFile(path.join(__dirname, '../public/html/index.html'));
 });/**/
 
+router.get('/logout', function(req, res, next) {
+    if (req.session) {
+      // delete session object
+      req.session.destroy(function(err) {
+        if(err) {
+          return next(err);
+        } else {
+          return res.redirect('/');
+        }
+      });
+    }else
+        res.redirect('/');
+  });
+
 module.exports = router;
