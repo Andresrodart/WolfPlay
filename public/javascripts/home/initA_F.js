f442816fbad8b7e79d3c4c502105a23b3ff82836
AndresRodart <arodartel1601@alumno.ipn.mx>
2018-06-01 03:01:29 -0500

    +F_A version

---
+++ public/javascripts/home/initA_F.js
@@ -0,0 +1,47 @@
+(function($){
+    $(function(){
+        $("#submit").on("submit",function  (e) {
+            e.preventDefault();
+            console.log();
+            $.post("/home/A_F", $(this).serialize(), function( data ) {
+                //if (data == 'succes' ) {
+                    //window.location.replace(`/home`);
+                //}
+                //else if(data == 'mailExist'){}
+                var xValues = new Array();
+                var yValues = new Array();
+                var yValues_ang = new Array();
+
+                for (let index = -10; index <= 10; index++) {
+                    xValues[index + 10] = index;
+                    yValues[index + 10] = data[index + 10].abs;
+                    yValues_ang[index + 10] = data[index + 10].ang;
+                }
+
+                var trace1 = {
+                    x: xValues,
+                    y: yValues,
+                    type: 'scatter'
+                };
+                var trace2 = {
+                    x: xValues,
+                    y: yValues_ang,
+                    type: 'scatter'
+                };
+
+                var data = [trace1];
+                var data_ang = [trace2];
+                Plotly.newPlot('plotAbs', data);
+                Plotly.newPlot('plotAng', data_ang);
+
+                var elements = document.querySelectorAll('.tit');
+                elements = elements.length ? elements : [elements];
+                for (var index = 0; index < elements.length; index++) {
+                    elements[index].style.visibility = 'visible';
+                }
+           });
+        })
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,8 @@ var path = require('path');
 var game_controller = require('../controllers/gameController');
 var Game = require('../models/game');
 var mongoose = require('mongoose');
+var math = require('mathjs');
+var Complex = require('complex.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,6 +13,32 @@ router.get('/', function(req, res, next) {
         res.redirect('/');
     }else
         res.render('home',{title: 'Home', user: req.session.user, mail: req.session.mail});//res.sendFile(path.join(__dirname, '../public/html/home.html'));//
+});
+
+router.get('/A_F', function(req, res, next){
+    if (!req.session.userId)
+        res.redirect('/');
+    res.render('A_F',{title: 'WolfPlay', user: req.session.user, mail: req.session.mail});
+});
+
+router.post('/A_F', function(req, res, next){
+    var a = req.body.numA;
+    var b = req.body.numB;
+
+    var c0 = new Complex({re:(a/2 + b/3),im: 0});
+    var exp = `f(n) = (${a}(-1 + (-1)^n * (1 - i*n*pi)))/(2 * pi^2 * n^2) + (${b} * (-1)^n * (pi * n * (2 + i * pi * n) + (2 * i * (-1)^n) - 2 * i))/(2 * pi^3 * n^3)`;
+    var matrix = new Array();
+
+    let parser = math.parser();
+    parser.eval(exp);
+    
+    for (let index = -10; index <= 10; index++){
+        let auxNum = new Complex(parser.eval(`f(${index})`));
+        matrix.push({abs: auxNum.abs(), ang: auxNum.arg()});
+    }
+    matrix[10] = {abs: c0.abs(), ang: c0.arg()};
+    return res.send(matrix);
+    //res.render('A_F',{title: 'WolfPlay', user: req.session.user, mail: req.session.mail, respons:true});
 });
 
 router.get('/:game', function(req, res, next) {
