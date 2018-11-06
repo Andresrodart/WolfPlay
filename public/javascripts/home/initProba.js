df53b6adbdb0e7f5a1a5a8fecab7006570aec121
Andresrodart <arodartel1601@alumno.ipn.mx>
2018-11-05 21:18:58 -0600

    Proba Validations

--- public/javascripts/home/initProba.js
+++ public/javascripts/home/initProba.js
@@ -9,5 +9,53 @@
            });
         })
 
+        $(".integrate").validate({
+            rules: {
+                numA: {
+                    required: true
+                },
+                numB: {
+                    required: true,
+                    min: function(element){
+                        console.log($("#numA").val() < $("#numB").val());
+                        return $("#numA").val() + 1;
+                    }
+                },
+                numR: {
+                    required: true,
+                    min: 0
+                },
+                numM: {
+                    required: true,
+                    min: 0
+                },
+            },
+            //For custom messages
+            messages: {
+                numA:{
+                    required: "ingresa el límite inferior",
+                },numB:{
+                    required: "ingresa el límite superior",
+                    min: "ingresa un número mayor al limite inferior"
+                },numR: {
+                    required: "ingresa sigma",
+                    min: "Ingresa un número positivo"
+                },numM: {
+                    required: "ingresa miu",
+                    min: "Ingresa un número positivo"
+                }
+            },
+            errorElement : 'div',
+            errorClass: 'invalid',//This will be the error for we would change
+            errorPlacement: function(error, element) {
+              var placement = $(element).data('invalid');
+              if (placement) {
+                $(placement).append(error)
+              } else {
+                error.insertAfter(element);
+              }
+            }
+        });
+
     }); // end of document ready
 })(jQuery); // end of jQuery name space
